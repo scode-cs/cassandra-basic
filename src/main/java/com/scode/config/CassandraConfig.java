613b9dff0dcde3a9640687542e05a3e40b9d47cf
santanu <santanu_cst@yahoo.com>
2022-02-01 13:12:45 +0530

    active cassandra config

--- src/main/java/com/scode/config/CassandraConfig.java
+++ src/main/java/com/scode/config/CassandraConfig.java
@@ -1,32 +1,37 @@
 package com.scode.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.cassandra.config.AbstractCassandraConfiguration;
 
+@Configuration
 public class CassandraConfig extends AbstractCassandraConfiguration {
 
-    @Value("${spring.data.cassandra.contact-points:placeholder}")
+    @Value("${spring.data.cassandra.contact-points}")
     private String contactPoints;
 
-    @Value("${spring.data.cassandra.port:0000}")
+    @Value("${spring.data.cassandra.port}")
     private int port;
 
-    @Value("${spring.data.cassandra.keyspace:placeholder}")
+    @Value("${spring.data.cassandra.keyspace-name}")
     private String keySpace;
 
 
     @Override
     protected String getKeyspaceName() {
-        return null;
+        System.out.println("keySpace: " + keySpace);
+        return keySpace;
     }
 
     @Override
     protected String getContactPoints() {
+        System.out.println("contactPoints: " + contactPoints);
         return contactPoints;
     }
 
     @Override
     protected int getPort() {
+        System.out.println("contactPoints: " + contactPoints);
         return port;
     }
 }
